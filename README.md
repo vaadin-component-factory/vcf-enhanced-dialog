7d96897c81c7f27c198183fd6293755b4b7db633
Nii Yeboah <nii@vaadin.com>
2020-05-12 18:15:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# &lt;vcf-element&gt;
+
+**Note that this is a starter boilerplate for creating Polymer 3 based components.**
+
+**Run `npm install` and follow the prompts to setup the workspace.**
+
+<!--
+[![Gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/vaadin/web-components?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
+[![npm version](https://badgen.net/npm/v/@vaadin-component-factory/vcf-element)](https://www.npmjs.com/package/@vaadin-component-factory/vcf-element)
+[![Published on Vaadin Directory](https://img.shields.io/badge/Vaadin%20Directory-published-00b4f0.svg)](https://vaadin.com/directory/component/vaadin-component-factoryvcf-element)
+
+[Live demo ↗](https://vcf-element.netlify.com)
+|
+[API documentation ↗](https://vcf-element.netlify.com/api/#/elements/Vaadin.VcfElement)
+-->
+
+## Installation
+
+Install `vcf-element`:
+
+```sh
+npm i @vaadin-component-factory/vcf-element --save
+```
+
+## Usage
+
+Once installed, import it in your application:
+
+```js
+import '@vaadin-component-factory/vcf-element';
+```
+
+Add `<vcf-element>` element to the page.
+
+```html
+<vcf-element></vcf-element>
+```
+
+## Running demo
+
+1. Fork the `vcf-element` repository and clone it locally.
+
+1. Make sure you have [npm](https://www.npmjs.com/) installed.
+
+1. When in the `vcf-element` directory, run `npm install` to install dependencies.
+
+1. Run `npm start` to open the demo.
+
+## Contributing
+
+To contribute to the component, please read [the guideline](https://github.com/vaadin/vaadin-core/blob/master/CONTRIBUTING.md) first.
+
+## Vaadin Prime
+
+This component is available in the Vaadin Prime subscription. It is still open source, but you need to have a valid CVAL license in order to use it. Read more at: https://vaadin.com/pricing
+
+## License
+
+Commercial Vaadin Add-on License version 3 (CVALv3). For license terms, see LICENSE.
+
+Vaadin collects development time usage statistics to improve this product. For details and to opt-out, see https://github.com/vaadin/vaadin-usage-statistics.
---
+++ src/vcf-element.js
@@ -0,0 +1,88 @@
+/**
+ * @license
+ * Copyright (C) 2015 Vaadin Ltd.
+ * This program is available under Commercial Vaadin Add-On License 3.0 (CVALv3).
+ * See the file LICENSE.md distributed with this software for more information about licensing.
+ * See [the website]{@link https://vaadin.com/license/cval-3} for the complete license.
+ */
+
+import { html, PolymerElement } from '@polymer/polymer/polymer-element';
+import { ThemableMixin } from '@vaadin/vaadin-themable-mixin';
+import { ElementMixin } from '@vaadin/vaadin-element-mixin';
+import '@vaadin/vaadin-license-checker/vaadin-license-checker';
+
+/**
+ * `<vcf-element>` --elementdescription--
+ *
+ * ```html
+ * <vcf-element></vcf-element>
+ * ```
+ *
+ * ### Styling
+ *
+ * The following custom properties are available for styling:
+ *
+ * Custom property | Description | Default
+ * ----------------|-------------|-------------
+ * `--vcf-element-property` | Example custom property | `unset`
+ *
+ * The following shadow DOM parts are available for styling:
+ *
+ * Part name | Description
+ * ----------------|----------------
+ * `part` | Example part
+ *
+ * The following state attributes are available for styling:
+ *
+ * Attribute    | Description | Part name
+ * -------------|-------------|------------
+ * `attribute` | Example styling attribute | :host
+ *
+ * @memberof Vaadin
+ * @mixes ElementMixin
+ * @mixes ThemableMixin
+ * @demo demo/index.html
+ */
+class VcfElement extends ElementMixin(ThemableMixin(PolymerElement)) {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+      </style>
+    `;
+  }
+
+  static get is() {
+    return 'vcf-element';
+  }
+
+  static get version() {
+    return '0.1.0';
+  }
+
+  static get properties() {
+    return {};
+  }
+
+  /**
+   * @protected
+   */
+  static _finalizeClass() {
+    super._finalizeClass();
+
+    const devModeCallback = window.Vaadin.developmentModeCallback;
+    const licenseChecker = devModeCallback && devModeCallback['vaadin-license-checker'];
+    if (typeof licenseChecker === 'function') {
+      licenseChecker(VcfElement);
+    }
+  }
+}
+
+customElements.define(VcfElement.is, VcfElement);
+
+/**
+ * @namespace Vaadin
+ */
+window.Vaadin.VcfElement = VcfElement;
